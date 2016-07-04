bb5093d66bc55a0c80617bed3931c5eb539a1985
austinsprague <sprague.ak@gmail.com>
2016-07-04 13:58:09 -0700

    checkout

--- app/checkout/controllers/checkout-controller.js
+++ app/checkout/controllers/checkout-controller.js
@@ -15,5 +15,27 @@
   function CheckoutCtrl() {
     var vm = this;
     vm.ctrlName = 'CheckoutCtrl';
+
+    vm.handler = StripeCheckout.configure({
+      key: 'pk_test_9Xzf0OH6f3wkWjcUhMPh9vtD',
+      image: '/img/documentation/checkout/marketplace.png',
+      locale: 'auto',
+      token: function(token) {
+        console.log(token);
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+        }
+    });
+
+    vm.button = function(e) {
+    // Open Checkout with further options:
+      handler.open({
+        name: 'Demo Site',
+        description: '2 widgets',
+        zipCode: true,
+        amount: 2000
+      });
+      e.preventDefault();
+    };
   }
 }());
