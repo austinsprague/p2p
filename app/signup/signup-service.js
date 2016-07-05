3e288166999d356da3ae925b2419bbe5123e62d3
austinsprague <sprague.ak@gmail.com>
2016-07-04 23:53:09 -0700

    sending to user_proj db

--- app/signup/signup-service.js
+++ app/signup/signup-service.js
@@ -10,23 +10,23 @@
     // delete $httpProvider.defaults.headers.common['X-Requested-With'];
 
     return {
-      // createUser: function (user) {
-      //   return $http.post('http://localhost:5000/api/signup', user).then(function(data) {
-      //     return data;
-      //   });
-      //   $scope.user = {};
-      //   $scope.createUser = function() {
-      //     $http({
-      //       method : 'POST',
-      //       url : 'http://localhost:5000/api/signup',
-      //       data : $scope.user
-      //     });
-      //     console.log($scope.user);
-      //   }
-      // }
-      // authUser: function(user) {
-      //   return $http.get('http://localhost:5000/auth')
-      // }
+      createUser: function (user) {
+        return $http.post('http://localhost:5000/api/signup', user).then(function(data) {
+          return data;
+        });
+        $scope.user = {};
+        $scope.createUser = function() {
+          $http({
+            method : 'POST',
+            url : 'http://localhost:5000/api/signup',
+            data : $scope.user
+          });
+          console.log($scope.user);
+        }
+      },
+      authUser: function(user) {
+        return $http.get('http://localhost:5000/auth/stripe')
+      }
     }
   }
 }());
