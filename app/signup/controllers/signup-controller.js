269e7be54ae8023daeb5da6d1880f6b74e750068
austinsprague <sprague.ak@gmail.com>
2016-07-06 01:09:29 -0700

    posting to server with account id

--- app/signup/controllers/signup-controller.js
+++ app/signup/controllers/signup-controller.js
@@ -12,7 +12,7 @@
     .module('signup')
     .controller('SignupCtrl', SignupCtrl);
 
-  function SignupCtrl($state, stripe, $http) {
+  function SignupCtrl($state, SignupService, stripe, $http) {
     var vm = this;
 
     vm.user = {};
@@ -24,11 +24,25 @@
         exp_year: vm.user.exp_year
       }, stripeResponseHandler);
 
+      vm.insertUser = {
+        first_name: vm.user.first_name,
+        last_name: vm.user.last_name,
+        address: vm.user.address,
+        city: vm.user.city,
+        state: vm.user.state,
+        phone_num: vm.user.phone_num,
+        background: vm.user.background,
+        email: vm.user.email,
+        pwd: vm.user.pwd,
+        zip: vm.user.zip
+        // token: vm.user.token
+      }
+
       function stripeResponseHandler(status, res) {
-        vm.user.token = {token: res.id};
-        $http.post('http://localhost:5000/api/signup', vm.user.token)
+        vm.insertUser.token = res.id;
+        $http.post('http://localhost:5000/api/signup', vm.insertUser)
           .then(function(data){
-            console.log(data);
+            console.log('this is the data' + data);
           }, function (err) {
             console.log(err);
           });
