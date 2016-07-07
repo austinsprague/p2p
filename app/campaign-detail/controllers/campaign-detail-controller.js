d8a399583152030b9f4016d45c79433b9fca6549
austinsprague <sprague.ak@gmail.com>
2016-07-07 12:04:30 -0700

    posting proj backed db

--- app/campaign-detail/controllers/campaign-detail-controller.js
+++ app/campaign-detail/controllers/campaign-detail-controller.js
@@ -12,14 +12,30 @@
     .module('campaignDetail')
     .controller('CampaignDetailCtrl', CampaignDetailCtrl);
 
-  function CampaignDetailCtrl($state, $stateParams, CampaignDetailService) {
+  function CampaignDetailCtrl($state, $stateParams, CampaignDetailService, $http) {
     var vm = this;
 
     CampaignDetailService.getProjectsById($stateParams.id).then(function(data) {
       vm.projectById = data;
       vm.company_name = data.company_name;
-      vm.img_url = data.img_url;
+      // vm.img_url = data.img_url;
       // console.log(vm.projectById);
-    })
+    });
+
+    vm.createUserCharge = function(){
+      console.log('clicked');
+      vm.userCharge = {};
+      vm.userCharge.backer_id = 1;
+      vm.userCharge.proj_id = 2;
+      vm.userCharge.amount = 1000;
+      vm.userCharge.user_id = 1;
+
+      $http.post('http://localhost:5000/api/user_projects/' + vm.userCharge.proj_id + '/charge', vm.userCharge)
+      .then(function(user){
+        console.log(user);
+        console.log('posted');
+        return user;
+      })
+    }
   }
 }());
